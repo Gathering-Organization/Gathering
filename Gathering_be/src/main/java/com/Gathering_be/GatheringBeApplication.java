80abd39eaedaca7b4b52a35277214b9188570b17
권용현 <gwon-yonghyeon@gwon-yonghyeon-ui-MacBookAir.local>
2025-01-17 18:15:14 +0900

    [Chore] 초기 설정 구성

---
+++ Gathering_be/src/main/java/com/Gathering_be/GatheringBeApplication.java
@@ -0,0 +1,13 @@
+package com.Gathering_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatheringBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatheringBeApplication.class, args);
+	}
+
+}
---
+++ Gathering_be/src/test/java/com/Gathering_be/GatheringBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Gathering_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatheringBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
