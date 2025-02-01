58366e68a42b9c97cba449855c43fe742e035814
Choi BoKeun <136104922+ChoiBoKeun1@users.noreply.github.com>
2025-02-01 11:46:38 +0900

    [fix] GET /api/project 요청 허용 추가 (#6)

--- Gathering_be/src/main/java/com/Gathering_be/global/config/SecurityConfig.java
+++ Gathering_be/src/main/java/com/Gathering_be/global/config/SecurityConfig.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -71,6 +72,7 @@ public class SecurityConfig {
                 .authorizeHttpRequests(authorizeRequest ->
                         authorizeRequest
                                 .requestMatchers(PERMITTED_API_URL).permitAll()
+                                .requestMatchers(HttpMethod.GET, "/api/project").permitAll()
                                 .anyRequest().authenticated()
                 )
                 .sessionManagement(sessionManagement ->
