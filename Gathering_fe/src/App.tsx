cb105c30472197a663862a08b45fdd610d0974f5
hi2242 <doodukly99@gmail.com>
2025-02-02 19:03:38 +0900

    [refactor] 모집글 작성(Posting) 코드 리팩토링

--- Gathering_fe/src/App.tsx
+++ Gathering_fe/src/App.tsx
@@ -3,18 +3,36 @@ import GoogleRedirectHandler from '@/components/GoogleRedirectHandler';
 import Profile from './components/Profile';
 import Main from './components/Main';
 import Layout from './components/Layout';
+import PostHome from './pages/PostHome';
+import Posting from './pages/Posting';
+import ViewPost from './pages/ViewPost';
+import PostEdit from './pages/PostEdit';
+import Notfound from './pages/Notfound';
+
+// 1. 모집글 관련 사이트
+// - "/postHome" : 모든 모집글들을 조회하는 PostHome 페이지
+// - "/posting" : 새로운 모집글을 작성하는 Posting 페이지
+// - "/viewPost" : 모집글을 볼 수 있는 ViewPost 페이지
+// - "/postEdit" : 모집글을 수정할 수 있는 postEdit 페이지
 
 const App: React.FC = () => {
   return (
-    <Router>
-      <Routes>
-        <Route path="/auth/google/callback" element={<GoogleRedirectHandler />} />
-        <Route element={<Layout />}>
-          <Route path="/" element={<Main />} />
-          <Route path="/profile" element={<Profile />} />
-        </Route>
-      </Routes>
-    </Router>
+    <>
+      <Router>
+        <Routes>
+          <Route path="/auth/google/callback" element={<GoogleRedirectHandler />} />
+          <Route element={<Layout />}>
+            <Route path="/" element={<Main />} />
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/postHome" element={<PostHome />} />
+            <Route path="/posting" element={<Posting />} />
+            <Route path="/viewPost/:id" element={<ViewPost />} />
+            <Route path="/postEdit/:id" element={<PostEdit />} />
+            <Route path="*" element={<Notfound />} />
+          </Route>
+        </Routes>
+      </Router>
+    </>
   );
 };
 
