9d0120ebb73231eaf04862e9592d4546dc0dd8b0
Choi BoKeun <136104922+ChoiBoKeun1@users.noreply.github.com>
2025-02-03 14:34:24 +0900

    [fix] projectMode BLENDED 추가 (#6)

--- Gathering_be/src/main/java/com/Gathering_be/global/enums/ProjectMode.java
+++ Gathering_be/src/main/java/com/Gathering_be/global/enums/ProjectMode.java
@@ -2,8 +2,9 @@ package com.Gathering_be.global.enums;
 
 public enum ProjectMode {
     ONLINE("온라인"),
-    OFFLINE("오프라인");
-
+    OFFLINE("오프라인"),
+    BLENDED("온+오프라인"),
+    ;
     private final String description;
 
     ProjectMode(String description) { this.description = description; }
