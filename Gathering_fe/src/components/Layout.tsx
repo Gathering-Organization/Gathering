55040964b8fb597e0886543f2def77bdfd2b5aa6
hi2242 <doodukly1999@gmail.com>
2025-02-18 21:25:38 +0900

    [feat] 하단 개발자 정보(Footer) 영역 생성

--- Gathering_fe/src/components/Layout.tsx
+++ Gathering_fe/src/components/Layout.tsx
@@ -1,11 +1,14 @@
 import { Outlet } from 'react-router';
 import Header from './Header';
+import Footer from './Footer';
 
 const Layout: React.FC = () => {
   return (
     <>
       <Header />
       <Outlet />
+      <div className="pb-[120px]"></div>
+      <Footer />
     </>
   );
 };
