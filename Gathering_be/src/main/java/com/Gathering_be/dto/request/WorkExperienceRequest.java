00ee13ea66f8ea8344b61036ec79ec7066804562
Choi BoKeun <136104922+ChoiBoKeun1@users.noreply.github.com>
2025-02-22 16:24:32 +0900

    [fix] WorkExperience jobDatail -> techStacks 변경 (#4)

--- Gathering_be/src/main/java/com/Gathering_be/dto/request/WorkExperienceRequest.java
+++ Gathering_be/src/main/java/com/Gathering_be/dto/request/WorkExperienceRequest.java
@@ -1,10 +1,12 @@
 package com.Gathering_be.dto.request;
 
+import com.Gathering_be.global.enums.TechStack;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import java.time.LocalDate;
+import java.util.Set;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
@@ -12,16 +14,16 @@ public class WorkExperienceRequest {
     private LocalDate startDate;
     private LocalDate endDate;
     private String activityName;
-    private String jobDetail;
+    private Set<TechStack> techStacks;
     private String description;
 
     @Builder
     public WorkExperienceRequest(LocalDate startDate, LocalDate endDate,
-                                 String activityName, String jobDetail, String description) {
+                                 String activityName, Set<TechStack> techStacks, String description) {
         this.startDate = startDate;
         this.endDate = endDate;
         this.activityName = activityName;
-        this.jobDetail = jobDetail;
+        this.techStacks = techStacks;
         this.description = description;
     }
 }
--- Gathering_be/src/main/java/com/Gathering_be/dto/response/WorkExperienceResponse.java
+++ Gathering_be/src/main/java/com/Gathering_be/dto/response/WorkExperienceResponse.java
@@ -1,11 +1,13 @@
 package com.Gathering_be.dto.response;
 
 import com.Gathering_be.domain.WorkExperience;
+import com.Gathering_be.global.enums.TechStack;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import java.time.LocalDate;
+import java.util.Set;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
@@ -13,16 +15,16 @@ public class WorkExperienceResponse {
     private LocalDate startDate;
     private LocalDate endDate;
     private String activityName;
-    private String jobDetail;
+    private Set<TechStack> techStacks;
     private String description;
 
     @Builder
     public WorkExperienceResponse(LocalDate startDate, LocalDate endDate,
-                                  String activityName, String jobDetail, String description) {
+                                  String activityName, Set<TechStack> techStacks, String description) {
         this.startDate = startDate;
         this.endDate = endDate;
         this.activityName = activityName;
-        this.jobDetail = jobDetail;
+        this.techStacks = techStacks;
         this.description = description;
     }
 
@@ -31,7 +33,7 @@ public class WorkExperienceResponse {
                 .startDate(experience.getStartDate())
                 .endDate(experience.getEndDate())
                 .activityName(experience.getActivityName())
-                .jobDetail(experience.getJobDetail())
+                .techStacks(experience.getTechStacks())
                 .description(experience.getDescription())
                 .build();
     }
