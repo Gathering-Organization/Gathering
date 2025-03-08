4cf91d730c59048f39c633b2610f12c4e7ccd145
Choi BoKeun <136104922+ChoiBoKeun1@users.noreply.github.com>
2025-03-08 17:43:20 +0900

    [refactor] 미사용 함수 제거 (#6)

--- Gathering_be/src/main/java/com/Gathering_be/repository/ProjectRepository.java
+++ Gathering_be/src/main/java/com/Gathering_be/repository/ProjectRepository.java
@@ -7,7 +7,6 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProjectRepository extends JpaRepository<Project, Long>, ProjectRepositoryCustom {
-    Page<Project> findAll(Pageable pageable);
     Page<Project> findAllByProfileNickname(String nickname, Pageable pageable);
     Page<Project> findAllByProfileNicknameAndIsClosed(String nickname, boolean isClosed, Pageable pageable);
 }
