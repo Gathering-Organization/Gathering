82e378976ef9091290e670db7991b9f0e02a82ac
hi2242 <doodukly1999@gmail.com>
2025-04-15 22:55:49 +0900

    [design] 모집글 수정하기 UI 완성

--- Gathering_fe/src/pages/PostEdit.tsx
+++ Gathering_fe/src/pages/PostEdit.tsx
@@ -9,6 +9,15 @@ import { getMyProfile } from '@/services/profileApi';
 import { ProfileInfo } from '@/types/profile';
 import { positionData } from '@/utils/position-data';
 import { techStacks } from '@/utils/tech-stacks';
+import SingleSelection from '@/components/SingleSelection';
+import TeamTagInput from '@/components/TeamTagInput';
+import {
+  durationOptions,
+  projectModeOptions,
+  projectTypeOptions,
+  totalMemberOptions
+} from '@/utils/post-options';
+import ReactQuill from 'react-quill';
 
 interface Position {
   id: string;
@@ -21,20 +30,6 @@ interface TechStack {
 }
 
 const PostEdit: React.FC = () => {
-  const nav = useNavigate();
-  const [startDate, setStartDate] = useState<{ startDate: Date | null; endDate: Date | null }>({
-    startDate: null,
-    endDate: null
-  });
-  const [deadline, setDeadline] = useState<{ startDate: Date | null; endDate: Date | null }>({
-    startDate: null,
-    endDate: null
-  });
-  const params = useParams();
-  const [positionList] = useState<Position[]>([...positionData]);
-  const [selectedPositions, setSelectedPositions] = useState<string[]>([]);
-  const [stackList] = useState<TechStack[]>([...techStacks]);
-  const [selectedStacks, setSelectedStacks] = useState<string[]>([]);
   const [post, setPost] = useState<PostingInfo>({
     title: '',
     description: '',
@@ -49,6 +44,27 @@ const PostEdit: React.FC = () => {
     teams: [],
     requiredPositions: []
   });
+  const nav = useNavigate();
+  const [startDate, setStartDate] = useState<{ startDate: Date | null; endDate: Date | null }>({
+    startDate: null,
+    endDate: null
+  });
+
+  const [deadline, setDeadline] = useState<{ startDate: Date | null; endDate: Date | null }>({
+    startDate: null,
+    endDate: null
+  });
+  const params = useParams();
+  const [projectType, setProjectType] = useState<string>(post.projectType);
+  const [selectedProjectMode, setSelectedProjectMode] = useState<string>(post.projectMode);
+  const [selectedTotalMembers, setSelectedTotalMembers] = useState<string>(
+    post.totalMembers.toString()
+  );
+  const [selectedDuration, setSelectedDuration] = useState<string>(post.duration);
+  const [positionList] = useState<Position[]>([...positionData]);
+  const [selectedPositions, setSelectedPositions] = useState<string[]>([...post.requiredPositions]);
+  const [stackList] = useState<TechStack[]>([...techStacks]);
+  const [selectedStacks, setSelectedStacks] = useState<string[]>([...post.techStacks]);
 
   const [info, setInfo] = useState<ProfileInfo>({
     nickname: '',
@@ -57,6 +73,31 @@ const PostEdit: React.FC = () => {
     techStacks: [],
     profileColor: ''
   });
+  useEffect(() => {
+    if (post.startDate) {
+      const parsedStart = new Date(`${post.startDate}T00:00:00`);
+      if (!isNaN(parsedStart.getTime())) {
+        setStartDate({ startDate: parsedStart, endDate: parsedStart });
+      }
+    }
+
+    if (post.deadline) {
+      const parsedDeadline = new Date(post.deadline);
+      if (!isNaN(parsedDeadline.getTime())) {
+        setDeadline({ startDate: parsedDeadline, endDate: parsedDeadline });
+      }
+    }
+  }, [post.startDate, post.deadline]);
+  useEffect(() => {
+    if (post) {
+      setProjectType(post.projectType);
+      setSelectedProjectMode(post.projectMode);
+      setSelectedTotalMembers(post.totalMembers.toString());
+      setSelectedDuration(post.duration);
+      setSelectedPositions(post.requiredPositions);
+      setSelectedStacks(post.techStacks);
+    }
+  }, [post]);
 
   const onUpdate = async () => {
     console.log('최종 저장 닉네임: ', post.teams);
@@ -150,187 +191,174 @@ const PostEdit: React.FC = () => {
   }, []);
 
   return (
-    <div className="p-4 bg-gray-50 min-h-screen">
-      <section className="bg-white p-6 rounded-lg shadow mb-4">
-        <label className="block font-semibold mb-2">
-          #{info.profileColor} {info.nickname}
-        </label>
-      </section>
-      <section className="bg-white p-6 rounded-lg shadow mb-4">
-        <label className="block font-semibold mb-2">제목</label>
+    <div className="mx-48 space-y-2">
+      <section className="p-6">
+        <label className="block font-semibold mb-4">제목</label>
         <input
           onChange={e => setPost({ ...post, title: e.target.value })}
+          type="text"
           value={post.title}
-          type="text"
           name="title"
-          className="border rounded w-full p-2"
+          className="border border-gray-300 rounded w-full p-4 mb-4 focus:outline-none"
+          placeholder="모집글의 제목을 입력하세요."
         />
       </section>
-      <section className="bg-white p-6 rounded-lg shadow mb-4">
-        <label className="block font-semibold mb-2">모집 구분</label>
-        <select
-          value={post.projectType}
-          onChange={e => setPost({ ...post, projectType: e.target.value })}
-          id="countries"
-          className="bg-white dark:bg-[#1E2028] border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-indigo-500 focus:border-indigo-500 block w-full p-2.5 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white"
+      <hr className="w-[1050px] justify-self-center border-[#000000]/15" />
+
+      <div className="grid grid-cols-2 gap-2 py-10">
+        <section className="px-6 py-2">
+          <label className="block mb-2">모집 구분</label>
+          <SingleSelection
+            title="모집 구분"
+            options={projectTypeOptions}
+            selectedValue={projectType}
+            setSelectedValue={value => {
+              setProjectType(value);
+              setPost(prev => ({ ...prev, projectType: value }));
+            }}
+          />
+        </section>
+        <section className="px-6 py-2">
+          <label className="block mb-2">진행 방식</label>
+          <SingleSelection
+            title="진행 방식"
+            options={projectModeOptions}
+            selectedValue={selectedProjectMode}
+            setSelectedValue={value => {
+              setSelectedProjectMode(value);
+              setPost(prev => ({ ...prev, projectMode: value }));
+            }}
+          />
+        </section>
+        <section className="px-6 py-2">
+          <label className="block mb-2">시작 예정</label>
+          <DatePicker
+            value={startDate}
+            onChange={newValue => {
+              console.log(startDate);
+              if (newValue) {
+                setStartDate(newValue);
+                setPost({ ...post, startDate: new Date(newValue.startDate || '').toISOString() });
+              }
+            }}
+            asSingle={true}
+            useRange={false}
+            inputClassName="text-gray-500 text-sm w-full cursor-pointer bg-gray-50 dark:bg-[#1E2028] border border-gray-300 rounded-[20px] p-3 px-6 pr-10 focus:outline-none"
+          />
+        </section>
+
+        <section className="px-6 py-2">
+          <label className="block mb-2">마감 날짜</label>
+          <DatePicker
+            value={deadline}
+            onChange={newValue => {
+              if (newValue) {
+                setDeadline(newValue);
+                setPost({ ...post, deadline: new Date(newValue.endDate || '').toISOString() });
+              }
+            }}
+            asSingle={true}
+            useRange={false}
+            inputClassName="text-gray-500 text-sm w-full cursor-pointer bg-gray-50 dark:bg-[#1E2028] border border-gray-300 rounded-[20px] p-3 px-6 pr-10 focus:outline-none"
+          />
+        </section>
+        <section className="px-6 py-2">
+          <label className="block mb-2">모집 인원</label>
+          <SingleSelection
+            title="모집 인원"
+            options={totalMemberOptions}
+            selectedValue={selectedTotalMembers}
+            setSelectedValue={(value: string) => {
+              setSelectedTotalMembers(value);
+              setPost(prev => ({ ...prev, totalMembers: Number(value) }));
+            }}
+          />
+        </section>
+
+        <section className="px-6 py-2">
+          <label className="block mb-2">카카오톡 오픈채팅 URL</label>
+          <input
+            onChange={e => setPost({ ...post, kakaoUrl: e.target.value })}
+            type="text"
+            value={post.kakaoUrl}
+            name="kakaoUrl"
+            className="border border-gray-300 rounded w-full placeholder-gray-500 bg-gray-50 rounded-[20px] p-3 px-6 text-sm focus:outline-none"
+            placeholder="카카오톡 오픈채팅 URL"
+          />
+        </section>
+        <section className="px-6 py-2">
+          <label className="block mb-2">팀원 태그</label>
+          <TeamTagInput teams={post.teams} setTeams={teams => setPost({ ...post, teams })} />
+        </section>
+        <section className="px-6 py-2">
+          <label className="block mb-2">예상 기간</label>
+          <SingleSelection
+            title="예상 기간"
+            options={durationOptions}
+            selectedValue={selectedDuration}
+            setSelectedValue={value => {
+              setSelectedDuration(value);
+              setPost(prev => ({ ...prev, duration: value }));
+            }}
+          />
+        </section>
+        <section className="px-6 py-2">
+          <label className="block mb-2">모집 포지션</label>
+          <MultiSelection
+            title="모집 포지션을 선택하세요."
+            options={positionList.map(pos => pos.title)}
+            selectedOptions={selectedPositions.map(
+              id => positionList.find(pos => pos.id === id)?.title || ''
+            )}
+            setSelectedOptions={selectedTitles => {
+              const selectedIds = positionList
+                .filter(pos => selectedTitles.includes(pos.title))
+                .map(pos => pos.id);
+
+              setSelectedPositions(selectedIds);
+              setPost({ ...post, requiredPositions: selectedIds });
+            }}
+          />
+        </section>
+        <section className="px-6 py-2">
+          <label className="block mb-2">사용 스택</label>
+          <MultiSelection
+            title="사용 스택을 선택하세요."
+            options={stackList.map(tech => tech.title)}
+            selectedOptions={selectedStacks.map(
+              id => stackList.find(tech => tech.id === id)?.title || ''
+            )}
+            setSelectedOptions={selectedTechs => {
+              const selectedIds = stackList
+                .filter(tech => selectedTechs.includes(tech.title))
+                .map(tech => tech.id);
+              setSelectedStacks(selectedIds);
+              setPost({ ...post, techStacks: selectedIds });
+            }}
+          />
+        </section>
+      </div>
+      <hr className="w-[1050px] justify-self-center border-[#000000]/15" />
+      <section className="p-6 mb-4">
+        <label className="block font-semibold mb-4">모집 소개</label>
+        <div className="h-[280px]">
+          <ReactQuill
+            theme="snow"
+            value={post.description}
+            onChange={(content: string) => setPost({ ...post, description: content })}
+            className="h-[240px] bg-white"
+            placeholder="모집 내용을 입력하세요..."
+          />
+        </div>
+      </section>
+      <div className="flex justify-center mt-6">
+        <button
+          className="bg-[#3387E5] font-bold px-10 py-2 text-white justify-items-center rounded-[30px] hover:bg-blue-600"
+          onClick={onUpdate}
         >
-          <option selected>모집 구분</option>
-          <option value="PROJECT">프로젝트</option>
-          <option value="CONTEST">대회</option>
-          <option value="STUDY">스터디</option>
-          <option value="OTHER">기타</option>
-        </select>
-      </section>
-      <section className="bg-white p-6 rounded-lg shadow mb-4">
-        <label className="block font-semibold mb-2">진행 방식</label>
-        <select
-          value={post.projectMode}
-          onChange={e => setPost({ ...post, projectMode: e.target.value })}
-          id="countries"
-          className="bg-white dark:bg-[#1E2028] border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-indigo-500 focus:border-indigo-500 block w-full p-2.5 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white"
-        >
-          <option selected>진행 방식</option>
-          <option value="ONLINE">온라인</option>
-          <option value="OFFLINE">오프라인</option>
-          <option value="BLENDED">온+오프라인</option>
-        </select>
-      </section>
-      <section className="bg-white p-6 rounded-lg shadow mb-4">
-        <label className="block font-semibold mb-2">모집 인원</label>
-        <input
-          value={post.totalMembers}
-          onChange={e => setPost({ ...post, totalMembers: Number(e.target.value) })}
-          type="text"
-          name="title"
-          className="border rounded w-full p-2"
-        />
-      </section>
-
-      <section className="bg-white p-6 rounded-lg shadow mb-4">
-        <label className="block font-semibold mb-2">시작 예정</label>
-        <DatePicker
-          value={startDate}
-          onChange={newValue => {
-            if (newValue) {
-              setStartDate(newValue);
-              setPost({ ...post, startDate: new Date(newValue.startDate || '').toISOString() });
-            }
-          }}
-          asSingle={true}
-          useRange={false}
-        />
-      </section>
-
-      <section className="bg-white p-6 rounded-lg shadow mb-4">
-        <label className="block font-semibold mb-2">마감 날짜</label>
-        <DatePicker
-          value={deadline}
-          onChange={newValue => {
-            if (newValue) {
-              setDeadline(newValue);
-              setPost({ ...post, deadline: new Date(newValue.endDate || '').toISOString() });
-            }
-          }}
-          asSingle={true}
-          useRange={false}
-        />
-      </section>
-      <section className="bg-white p-6 rounded-lg shadow mb-4">
-        <label className="block font-semibold mb-2">카카오톡 오픈채팅 URL</label>
-        <input
-          value={post.kakaoUrl}
-          onChange={e => setPost({ ...post, kakaoUrl: e.target.value })}
-          type="text"
-          name="kakaoUrl"
-          className="border rounded w-full p-2"
-        />
-      </section>
-      <section className="bg-white p-6 rounded-lg shadow mb-4">
-        <label className="block font-semibold mb-2">팀원 태그</label>
-        <input
-          value={post.teams.join(', ')}
-          onChange={e => {
-            setPost({
-              ...post,
-              teams: e.target.value.split(', ').map(nickname => nickname.trim())
-            });
-          }}
-          onBlur={() => {
-            setPost(prev => ({
-              ...prev,
-              teams: prev.teams.filter(nickname => nickname !== '')
-            }));
-          }}
-          type="text"
-          name="teams"
-          className="border rounded w-full p-2"
-        />
-      </section>
-
-      <section className="bg-white p-6 rounded-lg shadow mb-4">
-        <label className="block font-semibold mb-2">예상 기간</label>
-        <select
-          onChange={e => setPost({ ...post, duration: e.target.value })}
-          id="countries"
-          className="bg-white dark:bg-[#1E2028] border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-indigo-500 focus:border-indigo-500 block w-full p-2.5 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white"
-        >
-          <option selected>진행 방식</option>
-          <option value="ONE">1개월</option>
-          <option value="TWO">2개월</option>
-          <option value="THR">3개월</option>
-        </select>
-      </section>
-      <section className="bg-white p-6 rounded-lg shadow mb-4">
-        <label className="block font-semibold mb-2">모집 분야</label>
-        <MultiSelection
-          title="포지션 선택"
-          options={positionList.map(pos => pos.title)}
-          selectedOptions={selectedPositions.map(
-            id => positionList.find(pos => pos.id === id)?.title || ''
-          )}
-          setSelectedOptions={selectedTitles => {
-            const selectedIds = positionList
-              .filter(pos => selectedTitles.includes(pos.title))
-              .map(pos => pos.id);
-
-            setSelectedPositions(selectedIds);
-            setPost({ ...post, requiredPositions: selectedIds });
-          }}
-        />
-      </section>
-      <section className="bg-white p-6 rounded-lg shadow mb-4">
-        <label className="block font-semibold mb-2">사용 스택</label>
-        <MultiSelection
-          title="사용 스택 선택"
-          options={stackList.map(tech => tech.title)}
-          selectedOptions={selectedStacks.map(
-            id => stackList.find(tech => tech.id === id)?.title || ''
-          )}
-          setSelectedOptions={selectedTechs => {
-            const selectedIds = stackList
-              .filter(tech => selectedTechs.includes(tech.title))
-              .map(tech => tech.id);
-            setSelectedStacks(selectedIds);
-            setPost({ ...post, techStacks: selectedIds });
-          }}
-        />
-      </section>
-      <section className="bg-white p-6 rounded-lg shadow mb-4">
-        <label className="block font-semibold mb-2">모집 소개</label>
-        <input
-          value={post.description}
-          onChange={e => setPost({ ...post, description: e.target.value })}
-          type="text"
-          name="title"
-          className="border rounded w-full p-2"
-        />
-      </section>
-      <button
-        className="bg-blue-500 p-10 text-white w-full rounded hover:bg-blue-600"
-        onClick={onUpdate}
-      >
-        수정하기
-      </button>
+          작성하기
+        </button>
+      </div>
     </div>
   );
 };
