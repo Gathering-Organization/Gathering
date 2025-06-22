cfaf3d65b4fdc9dc863d44f08a1da615707f8583
ChoiBoKeun1 <136104922+ChoiBoKeun1@users.noreply.github.com>
2025-06-22 18:09:50 +0900

    [fix] 리포지토리 메서드 이름 변경 (#18)

--- Gathering_be/src/main/java/com/Gathering_be/repository/ApplicationRepository.java
+++ Gathering_be/src/main/java/com/Gathering_be/repository/ApplicationRepository.java
@@ -3,14 +3,12 @@ package com.Gathering_be.repository;
 import com.Gathering_be.domain.Application;
 import com.Gathering_be.domain.Project;
 import com.Gathering_be.global.enums.ApplyStatus;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface ApplicationRepository extends JpaRepository<Application, Long> {
-    List<Application> findByProjectId(Long projectId);
+    List<Application> findAllByProjectId(Long projectId);
     List<Application> findAllByProjectAndStatus(Project project, ApplyStatus status);
     boolean existsByProjectId(Long projectId);
 }
