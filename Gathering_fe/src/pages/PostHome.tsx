38455543a0a487d48c0ae85a3e69d02406cf3dd0
hi2242 <doodukly1999@gmail.com>
2025-08-29 21:37:58 +0900

    [design] 사용 설명서 배너 추가

--- Gathering_fe/src/pages/PostHome.tsx
+++ Gathering_fe/src/pages/PostHome.tsx
@@ -20,6 +20,7 @@ import OptionalDropdown from '@/components/OptionalDropdown';
 import { useToast } from '@/contexts/ToastContext';
 import EmblaCarouselComponent from '@/components/BannerCarousel';
 import banner1 from '/banner1.png';
+import banner2 from '/banner2.png';
 
 interface DropdownDispatchContextType {
   setSelectedStack: (value: string[]) => void;
@@ -56,12 +57,32 @@ const PostHome: React.FC = () => {
 
   const slides = [
     <div
+      onClick={() =>
+        window.open(
+          'https://diligent-cloudberry-302.notion.site/Gathering-25545ef4e6838014a912cd391552ab99?source=copy_link',
+          '_blank'
+        )
+      }
+      className="relative cursor-pointer h-48 sm:h-64 rounded-xl overflow-hidden shadow-lg group transform transition-transform duration-300 hover:scale-[1.005] will-change-transform"
+    >
+      <img src={banner2} alt="사용 설명서 배너" className="w-full h-full object-cover" />
+      <div className="absolute inset-16 flex flex-col items-start justify-center pl-6 sm:pl-10 text-[#fafbfc] text-left">
+        <h2 className="text-xl sm:text-3xl font-bold">
+          사용 방법을 배우면 프로젝트의 매칭 확률이 올라가요!
+        </h2>
+        <p className="text-sm sm:text-lg mt-2 font-medium">
+          게더링 서비스의 사용설명서 보러가기 &gt;&gt;
+        </p>
+      </div>
+    </div>,
+
+    <div
       onClick={() => window.open('https://github.com/Gathering-Organization/Gathering', '_blank')}
       className="relative cursor-pointer h-48 sm:h-64 rounded-xl overflow-hidden shadow-lg group transform transition-transform duration-300 hover:scale-[1.005] will-change-transform"
     >
-      <img src={banner1} alt="프로젝트 시작" className="w-full h-full object-cover" />
+      <img src={banner1} alt="깃허브 배너" className="w-full h-full object-cover" />
       <div className="absolute inset-16 flex flex-col items-start justify-center pl-6 sm:pl-10 text-[#fafbfc] text-left">
-        <h2 className="text-xl sm:text-3xl font-bold drop-shadow-lg">
+        <h2 className="text-xl sm:text-3xl font-bold">
           IT 초심자를 위한 팀원 모집 웹서비스, 게더링
         </h2>
         <p className="text-sm sm:text-lg mt-2 font-medium">
