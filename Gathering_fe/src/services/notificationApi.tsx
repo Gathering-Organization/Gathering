09ec8c9ebe601b153ef510b22aaaa9883a9c0cbb
hi2242 <doodukly1999@gmail.com>
2025-09-05 23:00:18 +0900

    [feat] 알림 API 함수 생성

---
+++ Gathering_fe/src/services/notificationApi.tsx
@@ -0,0 +1,81 @@
+import { api } from '@/services/api';
+import { PostingInfo } from '@/types/post';
+import { AxiosError } from 'axios';
+
+export const getNotification = async (nickname: string) => {
+  try {
+    const encodedNickname = encodeURIComponent(nickname);
+    const response = await api.get(`/notification?nickname=${encodedNickname}`);
+
+    if (response.data.status === 200) {
+      return { success: true, message: response.data.message, data: response.data.data };
+    }
+  } catch (error: unknown) {
+    console.error('알림 조회 실패:', error);
+
+    if (error instanceof AxiosError) {
+      console.error('서버 응답:', error.response?.data);
+    }
+
+    throw error;
+  }
+};
+
+export const getNotificationUnread = async (nickname: string) => {
+  try {
+    const encodedNickname = encodeURIComponent(nickname);
+    const response = await api.get(`/notification/unread-count?nickname=${encodedNickname}`);
+
+    if (response.data.status === 200) {
+      return { success: true, message: response.data.message, data: response.data.data };
+    }
+  } catch (error: unknown) {
+    console.error('미확인 알림 개수 조회 실패:', error);
+
+    if (error instanceof AxiosError) {
+      console.error('서버 응답:', error.response?.data);
+    }
+
+    throw error;
+  }
+};
+
+export const readNotification = async (nickname: string, notificationId: number) => {
+  try {
+    const encodedNickname = encodeURIComponent(nickname);
+    const response = await api.put(
+      `/notification/${notificationId}/read?nickname=${encodedNickname}`
+    );
+
+    if (response.data.status === 200) {
+      return { success: true, message: response.data.message, data: response.data.data };
+    }
+  } catch (error: unknown) {
+    console.error('알림 확인 실패:', error);
+
+    if (error instanceof AxiosError) {
+      console.error('서버 응답:', error.response?.data);
+    }
+
+    throw error;
+  }
+};
+
+export const readAllNotification = async (nickname: string) => {
+  try {
+    const encodedNickname = encodeURIComponent(nickname);
+    const response = await api.put(`/notification/read-all?nickname=${encodedNickname}`);
+
+    if (response.data.status === 200) {
+      return { success: true, message: response.data.message, data: response.data.data };
+    }
+  } catch (error: unknown) {
+    console.error('알림 확인 실패:', error);
+
+    if (error instanceof AxiosError) {
+      console.error('서버 응답:', error.response?.data);
+    }
+
+    throw error;
+  }
+};
